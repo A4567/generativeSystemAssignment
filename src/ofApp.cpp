3f3f2dbf2db35f27232331895965275a34ecd269
a4567 <archie.h.12@hotmail.co.uk>
2020-04-13 12:11:16 +0100

    updated with comments

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,35 +2,38 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-    
-    
-    
-    
+    //setting the background auto to false to keep old drawings and the colour to white as when the rectangles are drawn over it proves better for fading out the old lines
     ofSetBackgroundAuto(false);
     ofSetBackgroundColor(255);
+    //setup functions for the api and the contour finder
     setContour();
     setApi();
-    
+    //setting the frame rate to 20 - making the system a bit slower
     ofSetFrameRate(20);
     
 }
 void ofApp::setContour(){
+    //initialising the kinect - if using a camera this would be cam.setup()
+    cam.init();
+    cam.open();
+    //setting the width and height values for ease of use further on
     camW = cam.width;
     camH = cam.height;
     
-    cam.init();
-    cam.open();
+    //alloacting the image size for the contour finder images
     colorImg.allocate(camW,camH);
     grayImage.allocate(camW,camH);
     grayBg.allocate(camW,camH);
     grayDiff.allocate(camW,camH);
-    
+    //setting the threshold and number of contours
     thresh = 30;
     contnum = 1;
 }
 
 void ofApp::setApi(){
-    apiKey = "7mVxnXbbyWVMm4fXm4giYXGMb7WpTTmy8zAPuBRg";
+    //making the initial api call - api keys should be private - to acquire one visit the nasa api website
+    apiKey = "";
+    //making a string for the api call to use as the date
     string year = ofToString(ofGetYear());
     string month = ofToString(ofGetMonth());
     if(month.length() < 2){
@@ -50,10 +53,12 @@ void ofApp::setApi(){
 
 //--------------------------------------------------------------
 void ofApp::update(){
+    //keepign track of elapsed time
     time = ofGetElapsedTimef();
+    //clearing the line and the points vector to stop them growing too big
     line.clear();
-    
     blobpoints.clear();
+    //updating the camera feed and converting it to cv images to run through the contourfinder
     cam.update();
     if (cam.isFrameNew()){
         colorImg.setFromPixels(cam.getPixels());
@@ -72,12 +77,14 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    
+    // this scale and translation will depend on your output screen dimensions, i have done it here to have the feed in the middle of the screen the full height of it
     ofPushMatrix();
     ofTranslate(cam.width/2,0,0);
     ofScale(2, 2);
+    //the function to draw the outlines
     contour();
     ofPopMatrix();
+    //every 20s cover with a black rectangle whcih is see through
     if(time % 20 == 0){
         ofSetColor(0, 0, 0, 5);
         ofDrawRectangle(0,0, ofGetWidth(), ofGetHeight());
@@ -86,6 +93,7 @@ void ofApp::draw(){
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
+    //key press functions for testing and tuning - b changes the background to black, v to white, space resets the bgimg for the contour finder, p and q adjust the number of contours and w,o are the grey threshold values.
     if(key == 'b'){
         ofClear(0);
     }
@@ -111,8 +119,11 @@ void ofApp::keyPressed(int key){
     
 }
 //---
+//function to draw lines of contours found
 void ofApp::contour(){
+    //calls the api fucntion to add the points to the line
     api();
+    //if contours are found push the points to the blobpoints vector
     if(contourFinder.nBlobs > 0){
         for(int k = 0; k < contourFinder.nBlobs; k ++){
             for(int i = 0; i < contourFinder.blobs[k].pts.size(); i++){
@@ -120,11 +131,10 @@ void ofApp::contour(){
                 blobpoints.push_back(point);
             }
         }
-        time = ofGetElapsedTimef();
-        
+        //for every point in the vector create a color based on its location and add the point to the line with a curve
         for (int j = 0; j < blobpoints.size(); j++) {
             
-            ofColor faceColor;
+            ofColor lineColor;
             
             float pointAvg = (contourFinder.blobs[0].centroid.x + contourFinder.blobs[0].centroid.y)/2;//(blobpoints[j].y + blobpoints[j].x)/2;
             
@@ -134,11 +144,12 @@ void ofApp::contour(){
             float hue = ofMap(pointAvg, 0, camAvg, 0, 255);
             
             
-            faceColor.setHsb(hue, hue, 255, 5);
-            ofSetColor(faceColor);
+            lineColor.setHsb(hue, hue, 255, 5);
+            ofSetColor(lineColor);
             
             line.curveTo(blobpoints[j]);
         }
+        //close and draw the line
         line.close();
         
         
@@ -151,7 +162,7 @@ void ofApp::contour(){
 
 //--------------------------------------------------------------
 void ofApp::api(){
-    
+    // get the number of near earth objects gets values for velocity and close approach date these are used to determine the speed at which the points move using perline noise and the color the lines will be when no people are detected
     for(int i = 0; i < result["near_earth_objects"][date].size(); i++){
         
         string kps = result["near_earth_objects"][date][i]["close_approach_data"][0]["relative_velocity"]["kilometers_per_second"].asString();
@@ -165,14 +176,7 @@ void ofApp::api(){
         cad.erase(2,1);
         float hue = ofMap(ofToFloat(cad), 0000, 2359, 0, 255);
         ofColor colour;
-        colour.setHsb(0,0,255,10);
-        
-        string dMax = result["near_earth_objects"][date][i]["estimated_diameter"]["meters"]["estimated_diameter_max"].asString();
-        string dMin = result["near_earth_objects"][date][i]["estimated_diameter"]["meters"]["estimated_diameter_min"].asString();
-        float d = (ofToFloat(dMin) + ofToFloat(dMax))/2;
-        
-        string missDist = result["near_earth_objects"][date][i]["close_approach_data"][0]["miss_distance"]["astronomical"].asString();
-        float miss = ofToFloat(missDist);
+        colour.setHsb(hue,0,255,10);
         
         float posX = (i*1.3) + cam.getWidth()/2;
         float posY = (i*1.4) + cam.getHeight()/2;
@@ -184,17 +188,16 @@ void ofApp::api(){
             speed = v/100;
         }
         
-        float radius = d*miss;
         
         ofVec2f point;
         point.x = ((cam.getWidth()*2) * ofNoise(ofGetElapsedTimef() * speed + posX)) - cam.getWidth()/2;
         point.y = ((cam.getHeight()*2) * ofNoise(ofGetElapsedTimef()  * speed + posY)) - cam.getHeight()/2;
         
-        ofSetCircleResolution(360);
         
         line.curveTo(point.x, point.y);
         
     }
+    //if no contours are found it will draw the line anyway
     if(contourFinder.nBlobs < 1){
         line.close();
         line.draw();
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,5 +1,5 @@
 #pragma once
-
+//including the OpenCv addons dependencies along with the JSON and Kinect addons
 #include "ofMain.h"
 #include "ofxOpenCv.h"
 #include "opencv2/opencv.hpp"
@@ -12,34 +12,38 @@ class ofApp : public ofBaseApp{
 		void setup();
 		void update();
 		void draw();
-   
+   //custom functions for setting up the api and the contour drawing
     void api();
     void setApi();
-   
     void setContour();
+    void contour();
     
     void keyPressed(int key);
 	
-    void contour();
-    
+//using the kinect as the camera in this case as it is more mobile than my laptop webcam - could also use ofVideoGrabber instead
 
     ofxKinect cam;
-    ofImage camImg;
-    ofPixels camPix;
+    //width and height of the video source to set sizes for the cv images
+    int camW,camH;
     
+//setting the variables for the API functions - the url for the API call, the key and the date which will be set as the present day in the setup and the JSON elemnt which will read the returned data
     string url,apiKey,date;
     ofxJSONElement result;
     
 
-    
+    //the polyline which will be drawn - the contours and the api result
     ofPolyline line;
-   
-    int camW,camH;
+   // contour finder CV images
+    
     ofxCvColorImage colorImg;
     ofxCvGrayscaleImage grayImage, grayBg, grayDiff;
     ofxCvContourFinder contourFinder;
-    vector<ofVec3f> blobpoints;
+    //sets the background image to be compared by for finding contours
     bool bLearnBackground;
+    //values for tuning the contour finder - greyThreshold and the maximum contours to find
     int thresh,contnum;
+    //the points found in the contour outline which will be added to the polyline for drawing - this gets the contours found without the bounding rectangle
+    vector<ofVec3f> blobpoints;
+    // the elapsed time as an integer so it can be used to trigger the fade out
     int time;
 };
